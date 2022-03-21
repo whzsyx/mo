ea4a5b9712516cfadfb01fdd3c4bf81fc03a2a00
shangmo <55947868+whzsyx@users.noreply.github.com>
2022-03-21 21:11:00 +0800

    Update jd_wish.js

--- jd_wish.js
+++ jd_wish.js
@@ -25,8 +25,8 @@ let message = '', allMessage = '';
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-let appIdArr = ["1EFRQwA", "1E1xZy6s","1EFdRwqyF"];
-let appNameArr = ["疯狂砸金蛋","PLUS生活特权", "春尚新送好礼"];
+let appIdArr = ["1EFZWxKqP","1FFVQyqw","1EFRQwA", "1E1xZy6s","1EFdRwqyF"];
+let appNameArr = ["新品来袭","1111点心动","疯狂砸金蛋","PLUS生活特权", "春尚新送好礼"];
 let appId, appName;
 $.shareCode = [];
 if ($.isNode()) {
